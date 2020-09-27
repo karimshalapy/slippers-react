e24f910fa83dadcbbec731d0f71d07b3c0142d9e
Karim <karimshalapy@gmail.com>
2020-09-27 20:19:59 +0200

    feat: add GoBackBtn for the SideNavMenu

---
+++ src/layouts/BasicLayout/Header/SideNavMenu/GoBackBtn/GoBackBtn.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { SideMenuContext } from '../SideNavMenu';
+import { MenuTypes } from '../SideNavMenuTypes';
+import classes from './GoBackBtn.module.css';
+
+interface Props {
+    show: boolean
+}
+
+const GoBackBtn: React.FC<Props> = props => {
+
+    const { navigateMenuFunction, activeMenu } = useContext(SideMenuContext)
+    const btnClasses = [classes.GoBackBtn, props.show ? classes.ShowBtn : ""].join(" ")
+    const getGoBackText: () => [MenuTypes, string] = () => {
+        if (
+            activeMenu === "men" ||
+            activeMenu === "women" ||
+            activeMenu === "discover" ||
+            activeMenu === "discoverBack" ||
+            activeMenu === "gift"
+        ) return ["main", "main menu"]
+        else return ["discoverBack", "discover more"]
+    }
+    const [goBackType, goBackText] = getGoBackText()
+    return (
+        <div onClick={navigateMenuFunction.bind(null, goBackType, true)} className={btnClasses}>
+            <span className={classes.GoBackText}>{goBackText}</span>
+        </div>
+    )
+}
+
+export default GoBackBtn
