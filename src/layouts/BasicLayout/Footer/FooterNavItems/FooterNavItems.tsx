0264122e04b2c0ed2c48620f8917e3569d903066
Karim <karimshalapy@gmail.com>
2020-10-01 23:43:12 +0200

    refactor: change FooterNavItems to accept data as props
    Changed FooterNavItems to accept data as props and renders it dynamically
    depending on the type of the nav.
    removed the dummy data from this component.

--- src/layouts/BasicLayout/Footer/FooterNavItems/FooterNavItems.tsx
+++ src/layouts/BasicLayout/Footer/FooterNavItems/FooterNavItems.tsx
@@ -1,29 +1,52 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import { v4 } from 'uuid'
 import classes from './FooterNavItems.module.css'
+import { Data } from '../FooterTypes'
 
 interface Props {
-
+    type: "social" | "nav",
+    data: Data
 }
 
-const footerDummyData = [
-    { text: "sizing chart", url: "/sizing-chart" },
-    { text: "delivery & returns", url: "/delivery-returns" },
-    { text: "gift cards", url: "/gift-cards" },
-    { text: "careers", url: "/careers" },
-]
 
-const FooterNavItems: React.FC<Props> = props => {
-    return (
-        <ul className={classes.FooterNavItemsContainer}>
-            {footerDummyData.map(item => (
-                <li key={v4()} className={classes.FooterNavItem}>
-                    <Link to={item.url}>{item.text}</Link>
-                </li>
-            ))}
-        </ul>
-    )
+
+const FooterNavItems: React.FC<Props> = ({ type, data }) => {
+
+    const getNavItems = useCallback(() => {
+        switch (type) {
+            case "nav":
+                return (
+                    <ul className={classes.FooterNavItemsContainer}>
+                        {data.map(item => (
+                            <li key={v4()} className={classes.FooterNavItem}>
+                                <Link to={item.url}>{item.text}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                )
+            case "social":
+                return (
+                    <ul className={classes.SocialNavItemsContainer}>
+                        {data.map(item => (
+                            <li key={v4()} className={classes.SocialNavItem}>
+                                <a
+                                    href={item.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className={item.socialMediaIcon ? [classes[item.socialMediaIcon], classes.Social].join(" ") : ""}
+                                > </a>
+                            </li>
+                        ))}
+                    </ul>
+                )
+            default:
+                return null
+
+        }
+    }, [data, type])
+
+    return getNavItems()
 }
 
 export default FooterNavItems
---
+++ src/layouts/BasicLayout/Footer/FooterTypes.d.ts
@@ -0,0 +1,5 @@
+export type Data = {
+    text?: string,
+    url: string,
+    socialMediaIcon?: "Twitter" | "Facebook" | "Pinterest" | "Instagram",
+}[]
