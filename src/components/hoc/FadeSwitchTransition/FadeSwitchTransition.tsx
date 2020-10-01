72d2ed4e4259b74aac74c4155efd86abeb121a50
Karim <karimshalapy@gmail.com>
2020-10-01 21:14:21 +0200

    feat: add FadeSwitchTransition in a reusable hoc

---
+++ src/components/hoc/FadeSwitchTransition/FadeSwitchTransition.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement, useRef } from 'react'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import classes from './FadeSwitchTransition.module.css'
+
+interface Props {
+    switch: boolean,
+    children: (x: React.RefObject<HTMLDivElement>) => ReactElement
+}
+
+const FadeSwitchTransition: React.FC<Props> = props => {
+    const nodeRef = useRef<HTMLDivElement>(null)
+
+    return (
+        <SwitchTransition mode="out-in">
+            <CSSTransition
+                key={props.switch ? "subscribed" : "not subscribed"}
+                classNames={{
+                    enter: classes["fade-enter"],
+                    enterActive: classes["fade-enter-active"],
+                    exit: classes["fade-exit"],
+                    exitActive: classes["fade-exit-active"],
+                }}
+                addEndListener={done => nodeRef.current?.addEventListener("transitionend", done, false)}
+                nodeRef={nodeRef}
+                unmountOnExit
+                mountOnEnter
+            >
+                {props.children(nodeRef)}
+
+            </CSSTransition>
+        </SwitchTransition>
+    )
+}
+
+export default FadeSwitchTransition
