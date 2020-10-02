418dd45b4ca265eaa1bdb22905d818a9d4369757
Karim <karimshalapy@gmail.com>
2020-10-02 19:30:45 +0200

    feat: create redux store using thunk as middleware
    Created the redux store using the rootReducer that combines all reducers.
    also, used Thunk as middleware for the async dispatch actions.
    There's an composeEnhancers function that's required for redux dev tools.

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import { rootReducer } from './store/rootReducer/rootReducer'
+
+//delaring the type of redux dev tools extension to work with typescript
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+//compose Enhancers for redux devtools tweaked to work in development environment only
+const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
+
+//main store creation
+const store = createStore(
+  //the root reducer created by combineReducers above
+  rootReducer,
+  //compose enhancers for the redux dev tools and applying thunk middleware
+  composeEnhancers ? composeEnhancers(applyMiddleware(thunk)) : applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}> {/* using the provider component to allow child components to have access to redux store*/}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById('root')
 );
 
