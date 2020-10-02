dc3258ddb864900fa387788120db42341afe4fd1
Karim <karimshalapy@gmail.com>
2020-10-02 21:09:24 +0200

    refactor: use pressData from redux store instead of the dummy data

--- src/layouts/BasicLayout/Press/Press.tsx
+++ src/layouts/BasicLayout/Press/Press.tsx
@@ -1,45 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { v4 } from 'uuid'
 import classes from './Press.module.css'
-import { PressData } from './PressTypes'
+import { RootReducer } from '../../../store/rootReducer/reducersTypes'
 
-interface Props {
+const Press: React.FC = () => {
+    const pressData = useSelector((state: RootReducer) => state.mainResources.press)
 
-}
-
-const pressDummyData: PressData[] = [
-    {
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/press%2Flogo-independent.jpg?alt=media",
-        imgAlt: "independent logo"
-    },
-    {
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/press%2Flogo-gq.jpg?alt=media",
-        imgAlt: "GQ logo"
-    },
-    {
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/press%2Flogo-thesundaytimes.jpg?alt=media",
-        imgAlt: "the sunday times logo"
-    },
-    {
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/press%2Flogo-stylist.jpg?alt=media",
-        imgAlt: "stylist logo"
-    },
-    {
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/press%2Flogo-esquirre.jpg?alt=media",
-        imgAlt: "esquirre logo"
-    }
-]
-
-const Press: React.FC<Props> = props => {
     return (
         <section className={classes.Press}>
             <h2><span>mahabis</span> as seen in</h2>
             <ul className={classes.ImgsContainer}>
-                {pressDummyData.map((item, i, arr) => (
+                {pressData ? pressData.map((item, i, arr) => (
                     <li key={v4()}>
                         <img className={[classes.PressImage, i === arr.length - 1 ? "" : classes.PressImageSpacing].join(" ")} src={item.imgUrl} alt={item.imgAlt} />
                     </li>
-                ))}
+                )) : null}
             </ul>
         </section>
     )
