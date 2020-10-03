20c8068a18bfba7ee9485745e1f4ba77c494d2ec
Karim <karimshalapy@gmail.com>
2020-10-03 17:11:59 +0200

    fix: change switch case to a proper form

--- src/layouts/BasicLayout/Header/HeaderReducer.ts
+++ src/layouts/BasicLayout/Header/HeaderReducer.ts
@@ -13,18 +13,14 @@ export const reducer: HeaderReducer = (state, action) => {
     switch (action.type) {
         case "init":
             return initialState
-        case action.type:
-            if (action.type === "gift" ||
-                action.type === "men" ||
-                action.type === "women" ||
-                action.type === "discover") {
-
-                return {
-                    ...initialState,
-                    [action.type]: !state[action.type]
-                }
-            } else throw new Error("Wrong action type");
-
+        case "gift":
+        case "men":
+        case "women":
+        case "discover":
+            return {
+                ...initialState,
+                [action.type]: !state[action.type]
+            }
         default:
             throw new Error("Wrong action type")
     }
