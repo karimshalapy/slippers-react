5969e9ff69c8c76565b7ef274c4c8810c72b6f25
Karim <karimshalapy@gmail.com>
2020-10-06 16:56:12 +0200

    feat: add "404 page not found" page

---
+++ src/pages/_404/_404.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './_404.module.css'
+
+const _404: React.FC = () => {
+    return (
+        <section className={classes._404}>
+            <h1>Page not found</h1>
+            <p>We're sorry, but the page you're looking for could not be found. Go back to our <Link to="/">homepage</Link>, or check out some of our favourite items below:</p>
+        </section>
+    )
+}
+
+export default _404
