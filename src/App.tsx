f7022d8003c40477c4cd77de62a684a8a819c44b
Karim <karimshalapy@gmail.com>
2020-10-06 16:57:46 +0200

    feat: add routing as children inside BasicLayout in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import BasicLayout from './layouts/BasicLayout/BasicLayout';
+import Home from './pages/Home/Home';
+import _404 from './pages/_404/_404';
 import { getResources } from './store/actionsIndex/actionIndex'
 
 function App() {
@@ -15,7 +17,11 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <BasicLayout>
-          <p>Hello World</p>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/404" exact component={_404} />
+            <Redirect from="/" to="/404" />
+          </Switch>
         </BasicLayout>
       </div>
     </BrowserRouter>
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Home: React.FC<Props> = props => {
+    return (
+        <p>
+            Hello World!!
+        </p>
+    )
+}
+
+export default Home
