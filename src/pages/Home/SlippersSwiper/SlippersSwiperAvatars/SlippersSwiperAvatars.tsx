d52bb7eaa9e295e55127cf388857e7ec16062c02
Karim <karimshalapy@gmail.com>
2020-10-09 14:27:53 +0200

    refactor: use Slipper Avatar data from redux store instead of dummy data

--- src/pages/Home/SlippersSwiper/SlippersSwiperAvatars/SlippersSwiperAvatars.tsx
+++ src/pages/Home/SlippersSwiper/SlippersSwiperAvatars/SlippersSwiperAvatars.tsx
@@ -2,41 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SwiperCore from 'swiper'
 import { v4 } from 'uuid'
-import { SlipperSwiperAvatarData, SlippersTypesEnum, AvatarNavTypes, SlippersTypes } from '../SlippersSwiperTypes.d'
+import { SlippersTypesEnum, AvatarNavTypes, SlippersTypes } from '../SlippersSwiperTypes.d'
 import classes from './SlippersSwiperAvatars.module.css'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../../../store/rootReducer/reducersTypes'
 
 interface Props {
     controlledSwiper: SwiperCore | undefined,
     activeSlide: number
 }
-const avatarsDummyData: SlipperSwiperAvatarData[] = [
-    {
-        imgAlt: "mahabis classic in larvik light grey avatar image",
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/slippers-avatars%2Fclassic-avatar.jpg?alt=media",
-        type: "classic"
-    },
-    {
-        imgAlt: "Koge Grey Mahabis Canvas Slipper avatar image",
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/slippers-avatars%2Fcanvas-avatar.jpg?alt=media",
-        type: "canvas"
-    },
-    {
-        imgAlt: "mahabis flow in nora navy avatar image",
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/slippers-avatars%2Fflow-avatar.jpg?alt=media",
-        type: "flow"
-    },
-    {
-        imgAlt: "maabis luxe in borsen beige avatar image",
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/slippers-avatars%2Fluxe-avatar.jpg?alt=media",
-        type: "luxe"
-    },
-    {
-        imgAlt: "mahabis classic in larvik light grey from back avatar image",
-        imgUrl: "https://firebasestorage.googleapis.com/v0/b/slippers-react.appspot.com/o/slippers-avatars%2Fviewall-avatar.jpg?alt=media",
-        type: "view all",
-        url: "/"
-    }
-]
 
 const isSlippersTypes = (item: AvatarNavTypes): item is SlippersTypes => {
     return item !== "view all"
@@ -44,32 +18,36 @@ const isSlippersTypes = (item: AvatarNavTypes): item is SlippersTypes => {
 
 const SlippersSwiperAvatars: React.FC<Props> = props => {
 
+    const avatarsData = useSelector((state: RootReducer) => state.mainResources.slippersTypeSwiperAvatarImgs)
+
     return (
         <ul className={classes.Avatars}>
             {
-                avatarsDummyData.map(item => {
-                    const { type } = item
+                avatarsData ?
+                    avatarsData.map(item => {
+                        const { type } = item
 
-                    return (
-                        <li
-                            className={[classes.Avatar, isSlippersTypes(type) && props.activeSlide === SlippersTypesEnum[type] ? classes.ActiveAvatar : ""].join(" ")} key={v4()}
-                            onClick={isSlippersTypes(type) && props.controlledSwiper ? () => { props.controlledSwiper?.slideTo(SlippersTypesEnum[type]) } : () => { }}
-                        >
-                            {item.url
-                                ?
-                                <Link to={item.url}>
-                                    <img src={item.imgUrl} alt={item.imgAlt} />
-                                    <span className={classes.AvatarText}>{type}</span>
-                                </Link>
-                                :
-                                <>
-                                    <img src={item.imgUrl} alt={item.imgAlt} />
-                                    <p className={classes.AvatarText}>{type}</p>
-                                </>
-                            }
-                        </li>
-                    )
-                })
+                        return (
+                            <li
+                                className={[classes.Avatar, isSlippersTypes(type) && props.activeSlide === SlippersTypesEnum[type] ? classes.ActiveAvatar : ""].join(" ")} key={v4()}
+                                onClick={isSlippersTypes(type) && props.controlledSwiper ? () => { props.controlledSwiper?.slideTo(SlippersTypesEnum[type]) } : () => { }}
+                            >
+                                {item.url
+                                    ?
+                                    <Link to={item.url}>
+                                        <img src={item.imgUrl} alt={item.imgAlt} />
+                                        <span className={classes.AvatarText}>{type}</span>
+                                    </Link>
+                                    :
+                                    <>
+                                        <img src={item.imgUrl} alt={item.imgAlt} />
+                                        <p className={classes.AvatarText}>{type}</p>
+                                    </>
+                                }
+                            </li>
+                        )
+                    })
+                    : null
             }
         </ul>
     )
