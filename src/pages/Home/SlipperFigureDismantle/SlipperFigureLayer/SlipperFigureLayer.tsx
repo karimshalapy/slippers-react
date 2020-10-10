9af0bfeb661d21540e92303db99f57a2c6b4bc1b
Karim <karimshalapy@gmail.com>
2020-10-10 03:39:31 +0200

    feat: make a component for a single layer to accept and render LayerData

---
+++ src/pages/Home/SlipperFigureDismantle/SlipperFigureLayer/SlipperFigureLayer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './SlipperFigureLayer.module.css'
+import { LayerData } from '../SlipperFigureDismantleTypes.d'
+
+
+const SlipperFigureLayer: React.FC<LayerData> = props => {
+    return (
+        <figure className={`${classes.SlipperLayer} ${classes[props.layerType]}`} style={props.style}>
+            <img src={props.imgUrl} alt={props.imgAlt} />
+            <figcaption className={classes.FigureCaption}>
+                <div className={classes.CaptionTitle}>{props.title}</div>
+                <span>{props.text}</span>
+            </figcaption>
+        </figure>
+    )
+}
+
+export default SlipperFigureLayer
