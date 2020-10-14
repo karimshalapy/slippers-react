1217b872916a3fa2dde02cb3710fec356dee9a00
Karim <karimshalapy@gmail.com>
2020-10-14 19:46:38 +0200

    feat: use filterData from rootReducer instead of dummy data

--- src/pages/Slippers/FliterProducts/FilterSection/FilterSection.tsx
+++ src/pages/Slippers/FliterProducts/FilterSection/FilterSection.tsx
@@ -1,33 +1,57 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { FilterItemsTypes, GenderSizeFilterTypes } from '../../SlippersTypes.d'
 import classes from './FilterSection.module.css'
 
 interface Props {
-    title: string,
-    filterItems: string[]
+    title?: string,
+    filterItems?: FilterItemsTypes,
+    loading?: boolean
 }
 
-const FilterSection: React.FC<Props> = props => {
+const FilterSection: React.FC<Props> = ({ title, filterItems, loading }) => {
+
+    const isGenderSection = useCallback((filterItems: FilterItemsTypes): filterItems is GenderSizeFilterTypes => {
+        return typeof filterItems[0] !== "string"
+    }, [])
+
+    const getRenderData = useCallback(() => {
+        if (filterItems && !loading) {
+            if (!isGenderSection(filterItems)) {
+                return (
+                    <>
+                        <h3 className={classes.FilterSectionHeader}>{title}</h3>
+                        {
+                            filterItems.map(item => (
+                                <React.Fragment key={item}>
+                                    <input
+                                        type="checkbox"
+                                        id={`${title}-${item}`}
+                                        name={item}
+                                        className={classes.DefaultCheckBox}
+                                    />
+                                    <label
+                                        htmlFor={`${title}-${item}`}
+                                        className={classes.CheckboxLabel}
+                                    >
+                                        {item}
+                                    </label>
+                                </React.Fragment>
+                            ))
+
+                        }
+                    </>
+                )
+            } else {
+                return null
+            }
+        }
+        return null
+    }, [filterItems, loading, title, isGenderSection])
+
     return (
         <div>
-            <h3 className={classes.FilterSectionHeader}>{props.title}</h3>
             {
-                props.filterItems.map(item => (
-                    <React.Fragment key={item}>
-                        <input
-                            type="checkbox"
-                            id={`${props.title}-${item}`}
-                            name={`${props.title}-${item}`}
-                            className={classes.DefaultCheckBox}
-                        />
-                        <label
-                            htmlFor={`${props.title}-${item}`}
-                            className={classes.CheckboxLabel}
-                        >
-                            {item}
-                        </label>
-                    </React.Fragment>
-                ))
-
+                getRenderData()
             }
         </div>
     )
--- src/pages/Slippers/FliterProducts/FliterProducts.tsx
+++ src/pages/Slippers/FliterProducts/FliterProducts.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../../store/rootReducer/reducersTypes'
 import FilterSection from './FilterSection/FilterSection'
 import classes from './FliterProducts.module.css'
 
@@ -7,13 +9,23 @@ interface Props {
 }
 
 const FliterProducts: React.FC<Props> = props => {
+
+    const filterData = useSelector((state: RootReducer) => state.mainResources.slippers?.filterData)
+
     return (
         <form className={classes.FilterSection}>
             <h2>filters</h2>
-            <FilterSection title="shopping for:" filterItems={["men", "women", "helicopter"]} />
-            <FilterSection title="collections:" filterItems={["classic", "luxe", "canvas", "flow"]} />
-            <FilterSection title="upper colour:" filterItems={["beige", "black", "grey", "green", "white", "blue", "pink",]} />
-            <FilterSection title="sole colour:" filterItems={["beige", "white", "grey", "pink", "purple", "black", "yellow", "SYMC",]} />
+            {
+                filterData
+                    ?
+                    filterData.map(item => (
+                        <FilterSection key={item.title} title={item.title} filterItems={item.filterItems} />
+                    ))
+                    :
+                    [...Array(5)].map(() => (
+                        <FilterSection loading />
+                    ))
+            }
         </form>
     )
 }
