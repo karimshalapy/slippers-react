ea2a32e4942ac44295a792a7a83844828c39e6e7
Karim <karimshalapy@gmail.com>
2020-10-15 17:19:11 +0200

    feat: add GenderSizes enum and SlipperFilterState type to SlippersTypes
    Added the enum to use it to get the array of sizes in FilterSection.
    Added SlipperFilterState to describe the state of the filter form

--- src/pages/Slippers/SlippersTypes.d.ts
+++ src/pages/Slippers/SlippersTypes.d.ts
@@ -3,6 +3,7 @@ export type Gender = "men" | "women"
 export type UpperColorFilterTypes = "beige" | "black" | "grey" | "green" | "white" | "blue" | "pink"
 export type SoleColorFilterTypes = "beige" | "white" | "grey" | "pink" | "purple" | "black" | "yellow"
 export type ShortColorsTypes = "lig" | "sy" | "ss" | "pp" | "dg" | "sb" | "on" | "lw" | "aw" | "dk" | "js" | "lag" | "bg" | "kb" | "kg" | "ln" | "ks" | "as" | "fg" | "hp" | "ii"
+export type FilterSectionTypes = "gender" | "sizes" | "collection" | "upperColor" | "soleColor"
 export type Size = {
     eu: number,
     us: string,
@@ -13,9 +14,15 @@ export type Prices = {
     usd: number
 }
 export type GenderSizeFilterTypes = [Size[], Size[]]
+export enum GenderSizes { men, women }
 export type FilterItemsTypes = string[] | GenderSizeFilterTypes
 
+export type SlipperFilterState = {
+    [section in FilterSectionTypes]: string | null
+}
+
 export interface SlippersFilterData {
+    type: FilterSectionTypes,
     title: string,
     filterItems: FilterItemsTypes
 }
