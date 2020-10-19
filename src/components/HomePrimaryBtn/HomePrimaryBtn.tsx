5ee47462bfa1686ddf1c6caca96ff95759ebd673
Karim <karimshalapy@gmail.com>
2020-10-19 15:46:10 +0200

    feat: add outlined option to the HomePrimaryBtn reusable component

--- src/components/HomePrimaryBtn/HomePrimaryBtn.tsx
+++ src/components/HomePrimaryBtn/HomePrimaryBtn.tsx
@@ -4,18 +4,19 @@ import classes from './HomePrimaryBtn.module.css'
 interface Props {
     classNames?: string[],
     hasLink?: boolean
-    clickHandler?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    clickHandler?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
+    outlined?: boolean
 }
 
-const HomePrimaryBtn: React.FC<Props> = ({ classNames, hasLink, clickHandler, children }) => {
+const HomePrimaryBtn: React.FC<Props> = ({ classNames, hasLink, clickHandler, children, outlined }) => {
 
     const getBtnClasses = useCallback(() => {
-        const classesArr = [classes.HomePrimaryBtn]
+        const classesArr = [outlined ? classes.PrimaryButtonOutlined : classes.HomePrimaryBtn]
         if (!hasLink) classesArr.push(classes.NoLink)
         if (classNames) classesArr.push(...classNames)
 
         return classesArr.join(" ")
-    }, [classNames, hasLink])
+    }, [classNames, hasLink, outlined])
 
     return (
         <button className={getBtnClasses()} onClick={clickHandler}>
