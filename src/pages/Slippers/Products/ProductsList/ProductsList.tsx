011cf52e1a5d6a7d79355d26fb1a44945b3bb18e
Karim <karimshalapy@gmail.com>
2020-10-21 16:36:14 +0200

    refactor: use (if) to get render data instead of ternary operators

--- src/pages/Slippers/Products/ProductsList/ProductsList.tsx
+++ src/pages/Slippers/Products/ProductsList/ProductsList.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { v4 } from 'uuid'
@@ -11,14 +11,51 @@ interface Props {
     productsData: SlippersProductData[] | undefined
 }
 
-const ProductsList: React.FC<Props> = props => {
+const ProductsList: React.FC<Props> = ({ productsData }) => {
 
     const dispatch = useDispatch()
 
-    const linkClickHandler = (e: React.MouseEvent) => {
+    const linkClickHandler = useCallback((e: React.MouseEvent) => {
         e.preventDefault()
         dispatch(resetFilterState())
-    }
+    }, [dispatch])
+
+    const getRenderData = useCallback(() => {
+        if (productsData) {
+            if (productsData.length === 0) { //render "we're out of stock" when there're no available products"
+
+                return <li className={classes.OutOfStock}>we are sorry we are out of stock, please check our <a href="/slippers" onClick={linkClickHandler}>main collection</a> to view the full range</li>
+
+            } else { //render data when there're any available
+
+                return productsData.map(item => (
+                    <li className={classes.Product} key={v4()}>
+                        <Link to={`/${item.collection}-slipper?upper=${item.upperColorShortened}&sole=${item.soleColorShortened}`}>
+                            <div className={classes.Images}>
+                                <img src={item.mainImageUrl} alt={item.mainImageAlt} className={classes.MainImage} />
+                                <img src={item.secondaryImageUrl} alt={item.secondaryImageAlt} className={classes.SecondaryImage} />
+                            </div>
+                            <h3>mahabis {item.collection}</h3>
+                            <p dangerouslySetInnerHTML={{ __html: item.colorText }}></p>
+                            <data value={item.price.usd}>${item.price.usd}</data>
+                        </Link>
+                    </li>
+                ))
+            }
+        } else { //render loading placeholder blocks when the data is getting fetched from the server
+
+            return [...Array(10)].map(() => (
+                <li className={`${classes.Product} ${classes.Loading}`} key={v4()}>
+                    <div></div>
+                    <h3>&nbsp; </h3>
+                    <p>&nbsp; </p>
+                    <span>&nbsp; </span>
+                </li>
+            ))
+        }
+
+    }, [linkClickHandler, productsData])
+
 
     return (
 
@@ -27,37 +64,8 @@ const ProductsList: React.FC<Props> = props => {
                 <FadeSwitchTransition transitionKey={`products-${v4()}-filtered`} fast>
                     {
                         (nodeRef) => (
-                            <div ref={nodeRef} className={[classes.ProductsContainer, props.productsData?.length === 0 ? classes.NoProducts : ""].join(" ")}>
-                                {
-                                    props.productsData
-                                        ?
-                                        props.productsData.length === 0 //render "we're out of stock" when there're no available products"
-                                            ?
-                                            <li className={classes.OutOfStock}>we are sorry we are out of stock, please check our <a href="/slippers" onClick={linkClickHandler}>main collection</a> to view the full range</li>
-                                            :
-                                            props.productsData.map(item => ( //render data when there're any available
-                                                <li className={classes.Product} key={v4()}>
-                                                    <Link to={`/${item.collection}-slipper?upper=${item.upperColorShortened}&sole=${item.soleColorShortened}`}>
-                                                        <div className={classes.Images}>
-                                                            <img src={item.mainImageUrl} alt={item.mainImageAlt} className={classes.MainImage} />
-                                                            <img src={item.secondaryImageUrl} alt={item.secondaryImageAlt} className={classes.SecondaryImage} />
-                                                        </div>
-                                                        <h3>mahabis {item.collection}</h3>
-                                                        <p dangerouslySetInnerHTML={{ __html: item.colorText }}></p>
-                                                        <data value={item.price.usd}>${item.price.usd}</data>
-                                                    </Link>
-                                                </li>
-                                            ))
-
-                                        : [...Array(10)].map(() => ( //render loading placeholder blocks when the data is getting fetched from the server
-                                            <li className={`${classes.Product} ${classes.Loading}`} key={v4()}>
-                                                <div></div>
-                                                <h3>&nbsp; </h3>
-                                                <p>&nbsp; </p>
-                                                <span>&nbsp; </span>
-                                            </li>
-                                        ))
-                                }
+                            <div ref={nodeRef} className={[classes.ProductsContainer, productsData?.length === 0 ? classes.NoProducts : ""].join(" ")}>
+                                { getRenderData()}
                             </div>
                         )
                     }
