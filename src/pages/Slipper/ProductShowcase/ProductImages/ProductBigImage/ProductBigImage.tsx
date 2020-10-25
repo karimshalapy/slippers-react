e71e12655d6739d359828073fd8b30d335b24b16
Karim <karimshalapy@gmail.com>
2020-10-25 18:13:51 +0200

    refactor: seperate the active product big image into its own component

---
+++ src/pages/Slipper/ProductShowcase/ProductImages/ProductBigImage/ProductBigImage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { v4 } from 'uuid'
+import FadeSwitchTransition from '../../../../../components/hoc/FadeSwitchTransition/FadeSwitchTransition'
+import { SlippersProductData } from '../../../../Slippers/SlippersTypes'
+import classes from './ProductBigImage.module.css'
+
+interface Props {
+    activeSlipperData?: SlippersProductData,
+    activeImage?: string,
+}
+
+const ProductBigImage: React.FC<Props> = props => {
+    return (
+        <FadeSwitchTransition fast transitionKey={`active-image-${v4()}-changed`}>
+            {nodeRef => (
+                <div className={classes.ImageContainer} ref={nodeRef}>
+                    {
+                        props.activeSlipperData
+                            && props.activeImage
+                            && Object.keys(props.activeSlipperData.productShowcase).includes(props.activeImage)
+                            ?
+                            <img
+                                src={props.activeSlipperData.productShowcase[props.activeImage].imgUrl}
+                                alt={props.activeSlipperData.productShowcase[props.activeImage].imgAlt}
+                                className={classes.ProductBigImage}
+                            />
+                            : null
+                    }
+                </div>
+            )}
+        </FadeSwitchTransition>
+    )
+}
+
+export default ProductBigImage
--- src/pages/Slipper/ProductShowcase/ProductImages/ProductImages.tsx
+++ src/pages/Slipper/ProductShowcase/ProductImages/ProductImages.tsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import ProductSmallImages from './ProductSmallImages/ProductSmallImages'
 import classes from './ProductImages.module.css'
 import { SlippersProductData } from '../../../Slippers/SlippersTypes'
-import FadeSwitchTransition from '../../../../components/hoc/FadeSwitchTransition/FadeSwitchTransition'
-import { v4 } from 'uuid'
+import ProductBigImage from './ProductBigImage/ProductBigImage'
 
 interface Props {
     activeSlipperData?: SlippersProductData
@@ -30,22 +29,7 @@ const PrdouctImages: React.FC<Props> = ({ activeSlipperData }) => {
                     />
                     : null
             }
-            <FadeSwitchTransition fast transitionKey={`active-image-${v4()}-changed`}>
-                {nodeRef => (
-                    <div className={classes.ImageContainer} ref={nodeRef}>
-                        {
-                            activeSlipperData && activeImage && Object.keys(activeSlipperData.productShowcase).includes(activeImage)
-                                ?
-                                <img
-                                    src={activeSlipperData.productShowcase[activeImage].imgUrl}
-                                    alt={activeSlipperData.productShowcase[activeImage].imgAlt}
-                                    className={classes.ProductBigImage}
-                                />
-                                : null
-                        }
-                    </div>
-                )}
-            </FadeSwitchTransition>
+            <ProductBigImage activeSlipperData={activeSlipperData} activeImage={activeImage} />
         </div>
     )
 }
