b0812def3a0934b891cf35af6606ffc5b083c255
Karim <karimshalapy@gmail.com>
2020-10-26 18:47:33 +0200

    feat: add sole color field styling with all colors #hex

--- src/pages/Slipper/ProductShowcase/ProductDetails/SoleColors/SoleColors.tsx
+++ src/pages/Slipper/ProductShowcase/ProductDetails/SoleColors/SoleColors.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SlippersProductData } from '../../../../Slippers/SlippersTypes'
+import classes from './SoleColors.module.css'
 
 interface Props {
     pageProductsData?: SlippersProductData[],
@@ -10,13 +11,14 @@ interface Props {
 
 const SoleColors: React.FC<Props> = props => {
     return (
-        <>
+        <div className={classes.SoleColrosContainer}>
             {
                 props.pageProductsData && props.activeUpperColor
                     ?
                     props.pageProductsData.filter(item => item.upperColorLongText === props.activeUpperColor).map(item => (
                         <React.Fragment key={item.soleColorLongText}>
                             <input
+                                className={classes.RadioButton}
                                 type="radio"
                                 name="soleColor"
                                 id={`sole-${item.soleColorLongText}`}
@@ -24,13 +26,13 @@ const SoleColors: React.FC<Props> = props => {
                                 checked={props.activeSoleColor === item.soleColorLongText}
                                 onChange={props.updateGlobalActiveColorState.bind(null, item.soleColorLongText, "sole")}
                             />
-                            <label htmlFor={`sole-${item.soleColorLongText}`}>{item.soleColorLongText}</label>
+                            <label className={`${classes.SoleColor} ${classes[item.soleColorShortened]}`} htmlFor={`sole-${item.soleColorLongText}`}></label>
                         </React.Fragment>
                     ))
                     :
                     null
             }
-        </>
+        </div>
     )
 }
 
