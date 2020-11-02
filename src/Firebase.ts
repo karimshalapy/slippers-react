d24f2b40d7990e857cf0e06c034549af83fed35e
Karim Shalapy <karimshalapy@gmail.com>
2020-11-02 16:12:50 +0200

    feat: install firebase module and initialize firebase app

---
+++ src/Firebase.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+    apiKey: "AIzaSyAdwnFOhI279kHXnBDKySpKvUpAYP46ETg",
+    authDomain: "slippers-react.firebaseapp.com",
+    databaseURL: "https://slippers-react.firebaseio.com",
+    projectId: "slippers-react",
+    storageBucket: "slippers-react.appspot.com",
+    messagingSenderId: "154594899645",
+    appId: "1:154594899645:web:f95f46499c34e60484a9d4",
+    measurementId: "G-8CY1LMVPLD"
+};
+const app = firebase.initializeApp(config)
+export default app
+export const auth = app.auth()
+export const {
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    GithubAuthProvider,
+    TwitterAuthProvider,
+    EmailAuthProvider
+} = firebase.auth
