2e663754c7a3d94bf7df197d2219328b151da09c
Karim Shalapy <karimshalapy@gmail.com>
2020-11-04 18:54:45 +0200

    feat: use react hook forms in Auth forms with validation schemes

--- src/pages/Auth/Signup/Signup.tsx
+++ src/pages/Auth/Signup/Signup.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputWithLabelAsPlaceholder from '../../../components/InputWithLabelAsPlaceholder/InputWithLabelAsPlaceholder'
 import Button from '../../../components/Button/Button'
 import classes from './Signup.module.css'
 import MobileSwitchPanelButton from '../MobileSwitchPanelButton/MobileSwitchPanelButton'
+import { SignupFormInputs } from '../AuthTypes'
+import { auth } from '../../../Firebase'
+import { useForm } from 'react-hook-form'
+import { singupSchema } from '../AuthFormsValidation'
 
 interface Props {
     isSignup: boolean
@@ -10,15 +14,48 @@ interface Props {
 }
 
 const Signup: React.FC<Props> = props => {
+
+    const [signupSubmitError, setSignupSubmitError] = useState<string>()
+
+    const { register, handleSubmit, errors } = useForm<SignupFormInputs>({
+        mode: "onChange",
+        resolver: singupSchema
+    })
+
+    const signUpHandler = (data: SignupFormInputs) => {
+        auth.createUserWithEmailAndPassword(data.email, data.password)
+            .then(userData => userData.user?.updateProfile({ displayName: data.name }))
+            .then(() => setSignupSubmitError(undefined))
+            .catch(err => setSignupSubmitError(err.message))
+    }
     return (
         <div className={[classes.SignupContainer, props.isSignup ? classes.RightPanelActive : " "].join(" ")}>
-            <form action="#">
+            <form onSubmit={handleSubmit(signUpHandler)}>
                 <h1>Create Account</h1>
 
                 <span>or use your email for registration</span>
-                <InputWithLabelAsPlaceholder inputType="text" labelText="Name" />
-                <InputWithLabelAsPlaceholder inputType="email" labelText="Email" />
-                <InputWithLabelAsPlaceholder inputType="password" labelText="Password" />
+                <InputWithLabelAsPlaceholder
+                    inputType="text"
+                    labelText="Name"
+                    inputName="name"
+                    error={errors.name?.message}
+                    ref={register}
+                />
+                <InputWithLabelAsPlaceholder
+                    inputType="email"
+                    labelText="Email"
+                    inputName="email"
+                    error={errors.email?.message}
+                    ref={register}
+                />
+                <InputWithLabelAsPlaceholder
+                    inputType="password"
+                    labelText="Password"
+                    inputName="password"
+                    error={errors.password?.message}
+                    ref={register}
+                />
+                {signupSubmitError ? <p className={classes.SubmitErrorMessage}>{signupSubmitError}</p> : null}
                 <Button>Sign Up</Button>
                 <MobileSwitchPanelButton switchPanelHandler={props.switchPanelHandler} panelType="signup" />
             </form>
