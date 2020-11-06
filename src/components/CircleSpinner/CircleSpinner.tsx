fed40f00700dd19278732557acce646c4d319326
Karim Shalapy <karimshalapy@gmail.com>
2020-11-06 16:13:36 +0200

    feat: add circle spinner component to render when loading

---
+++ src/components/CircleSpinner/CircleSpinner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './CircleSpinner.module.css'
+
+interface Props {
+    size: number,
+}
+
+const CircleSpinner: React.FC<Props> = props => {
+    return (
+        <svg className={classes.Spinner} viewBox="0 0 50 50" style={{ width: props.size, height: props.size }}>
+            <circle className={classes.Path} cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        </svg>
+    )
+}
+
+export default CircleSpinner
