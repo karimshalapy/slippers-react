a95e8442e659028bc90f17d1780e71c5bc29afd0
Karim Shalapy <karimshalapy@gmail.com>
2020-11-06 20:59:46 +0200

    fix: change stroke-width attribute to the proper strokeWidth

--- src/components/CircleSpinner/CircleSpinner.tsx
+++ src/components/CircleSpinner/CircleSpinner.tsx
@@ -8,7 +8,7 @@ interface Props {
 const CircleSpinner: React.FC<Props> = props => {
     return (
         <svg className={classes.Spinner} viewBox="0 0 50 50" style={{ width: props.size, height: props.size }}>
-            <circle className={classes.Path} cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+            <circle className={classes.Path} cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
         </svg>
     )
 }
