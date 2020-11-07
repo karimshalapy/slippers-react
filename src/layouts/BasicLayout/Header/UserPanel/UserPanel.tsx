eec27c3fd2a45c56dcb5ec2a7f802e600fe16d27
Karim Shalapy <karimshalapy@gmail.com>
2020-11-07 16:27:06 +0200

    feat: add cart icon to render when user is logged in

--- src/layouts/BasicLayout/Header/UserPanel/UserPanel.tsx
+++ src/layouts/BasicLayout/Header/UserPanel/UserPanel.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import { ReactComponent as User } from '../../../../assets/user-regular.svg'
+import { ReactComponent as Cart } from '../../../../assets/shopping-cart.svg'
 import classes from './UserPanel.module.css'
 import UserPanelDropdown from './UserPanelDropdown/UserPanelDropdown'
+import { FirebaseUserContext } from '../../../../App'
 
 interface Props {
 
@@ -11,13 +13,23 @@ interface Props {
 const UserPanel: React.FC<Props> = props => {
     const [dropdownActive, setDropdownActive] = useState(false)
     const location = useLocation()
+    const user = useContext(FirebaseUserContext)
 
     useEffect(() => setDropdownActive(false), [location.pathname])
 
     return (
-        <div className={`${classes.UserPanelContainer} ${location.pathname === "/auth" ? classes.Hide : ""}`}>
-            <div className={classes.UserIconContainer} onClick={() => setDropdownActive(prev => !prev)}>
-                <User className={classes.UserIcon} />
+        <div className={classes.UserPanelContainer}>
+            <Link
+                to="/cart"
+                className={`${classes.IconContainer} ${location.pathname === "/cart" || !user ? classes.Hide : ""}`}
+            >
+                <Cart className={classes.Icon} />
+            </Link>
+            <div
+                onClick={() => setDropdownActive(prev => !prev)}
+                className={`${classes.IconContainer} ${location.pathname === "/auth" ? classes.Hide : ""}`}
+            >
+                <User className={classes.Icon} />
             </div>
             <UserPanelDropdown dropdownActive={dropdownActive} setDropdownActive={setDropdownActive} />
         </div>
