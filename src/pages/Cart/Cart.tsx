5cb90dae36098d45d39145e77e296adda499b353
Karim Shalapy <karimshalapy@gmail.com>
2020-11-09 17:42:50 +0200

    feat: render CartItems in Cart component

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../@types/reducersTypes'
 import { FirebaseUserContext } from '../../App'
 import ScrollToTopOnPathChange from '../../components/ScrollToTopOnPathChange/ScrollToTopOnPathChange'
 import classes from './Cart.module.css'
+import CartItems from './CartItems/CartItems'
 
 interface Props {
 
@@ -9,16 +12,20 @@ interface Props {
 
 const Cart: React.FC<Props> = props => {
 
+    const cartItemsEntries = useSelector((state: RootReducer) => Object.entries(state.cartData.cartItems))
     const user = useContext(FirebaseUserContext)
 
     return (
         <>
             <ScrollToTopOnPathChange />
             <section className={classes.CartContainer}>
-                <div>
+                <form>
                     <h2>welcome, {user?.displayName}!</h2> <span>not you?</span>
                     <h3>your cart</h3>
-                </div>
+                    <div className={classes.CartInfoContainer}>
+                        <CartItems cartItemsEntries={cartItemsEntries} />
+                    </div>
+                </form>
             </section>
         </>
     )
