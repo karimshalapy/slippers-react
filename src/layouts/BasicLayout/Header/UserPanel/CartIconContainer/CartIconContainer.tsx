bfec0b09cc0a505fc10ebf116a87e0168b760dc4
Karim Shalapy <karimshalapy@gmail.com>
2020-11-10 20:49:19 +0200

    feat: add counter for the cart icon in header nav

--- src/layouts/BasicLayout/Header/UserPanel/CartIconContainer/CartIconContainer.tsx
+++ src/layouts/BasicLayout/Header/UserPanel/CartIconContainer/CartIconContainer.tsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react'
+import { useSelector } from 'react-redux'
 import { Link, useLocation } from 'react-router-dom'
+import { RootReducer } from '../../../../../@types/reducersTypes'
 import { FirebaseUserContext } from '../../../../../App'
 import { ReactComponent as CartIcon } from '../../../../../assets/shopping-cart.svg'
+import classes from './CartIconContainer.module.css'
 
 interface Props {
     hide: boolean,
@@ -14,12 +17,16 @@ const CartIconContainer: React.FC<Props> = props => {
 
     const location = useLocation()
     const user = useContext(FirebaseUserContext)
+    const cartItemsCount = useSelector((state: RootReducer) => {
+        return Object.values(state.cartData.cartItems).reduce((prev, { amount }) => prev + amount, 0)
+    })
 
     return (
         <Link
             to="/cart"
             className={`${props.iconContainerClass} ${location.pathname === "/cart" || !user || props.hide ? props.hideIconClass : ""}`}
         >
+            {cartItemsCount > 0 ? <span className={classes.CartItemsCounter}>{cartItemsCount}</span> : null}
             <CartIcon className={props.iconClass} />
         </Link>
     )
