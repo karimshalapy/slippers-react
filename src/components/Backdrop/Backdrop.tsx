34c07f4bf11e9bfe585df5e6a8c5c06cedd65ef3
Karim Shalapy <karimshalapy@gmail.com>
2020-11-11 17:18:32 +0200

    feat: make the Backdrop component accept z-index as props

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -3,11 +3,12 @@ import classes from './Backdrop.module.css'
 
 interface Props {
     show: boolean,
-    clickHandler: (e: React.MouseEvent) => void
+    clickHandler: (e: React.MouseEvent) => void,
+    zIndex?: number
 }
 
 const Backdrop: React.FC<Props> = props => {
-    return props.show ? <div className={classes.Backdrop} onClick={props.clickHandler}></div> : null
+    return props.show ? <div style={{ zIndex: props.zIndex }} className={classes.Backdrop} onClick={props.clickHandler}></div> : null
 }
 
 export default Backdrop
