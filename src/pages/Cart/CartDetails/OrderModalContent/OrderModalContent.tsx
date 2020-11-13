fbb1bb12e2954f1d4baf4f983516fc47aef12eba
Karim Shalapy <karimshalapy@gmail.com>
2020-11-13 19:28:41 +0200

    refactor: change modal section styling to be in Modal.module.css

--- src/pages/Cart/CartDetails/OrderModalContent/OrderModalContent.tsx
+++ src/pages/Cart/CartDetails/OrderModalContent/OrderModalContent.tsx
@@ -2,7 +2,6 @@ import React, { memo, useCallback } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import CircleSpinner from '../../../../components/CircleSpinner/CircleSpinner'
 import Button from '../../../../components/Button/Button'
-import classes from './OrderModalContent.module.css'
 
 interface Props {
     orderLoading: boolean,
@@ -29,7 +28,7 @@ const OrderModal: React.FC<Props> = ({
 
     if (orderLoading) return <CircleSpinner size={50} />
     if (orderSuccess) return (
-        <section className={classes.OrderModalContentContainer}>
+        <section>
             <h2>Order Success</h2>
             <p>Thank you for buying from us <span role="img" aria-label="love">&#10084;&#65039;</span></p>
             <Button ghost clickHandler={clearCartAndRedirect}>continue shopping &amp; clear cart</Button>
@@ -37,7 +36,7 @@ const OrderModal: React.FC<Props> = ({
         </section>
     )
     if (orderError) return (
-        <section className={classes.OrderModalContentContainer}>
+        <section>
             <h2>Order Failed"</h2>
             <p>something went wrong. Please try again later</p>
             <Button ghost clickHandler={resetOrder}>try again</Button>
